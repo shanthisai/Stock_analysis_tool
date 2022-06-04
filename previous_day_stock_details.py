0574cac45d236e03c65a2bb888d01c2156811200
shanthisai <72120417+shanthisai@users.noreply.github.com>
2022-06-04 18:43:59 +0530

    Previous_day py file upload

---
+++ previous_day_stock_details.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+
+def previous_day_stock_details(symbol,each_stock_df,prev_day_dict):
+    # each_stock_df["Date"] = pd.to_datetime(each_stock_df["Date"])
+    last_day_list = (each_stock_df.iloc[0].to_list())
+    for i in range(len(last_day_list[:-1])):
+        each_col_list = prev_day_dict.get(columns[i],[])
+        each_col_list.append(last_day_list[i])
+        prev_day_dict[columns[i]] = each_col_list
+    # print(prev_day_dict)
+    return prev_day_dict
+
+path = os.path.join(os.getcwd(), "NASDAQ_Data")
+df = pd.read_csv(os.path.join(path, "equity_data.csv"))
+stock_data_path = os.path.join(path, "Stock_data")
+sorted_df = df.sort_values(by=["Market Cap"], ascending=False)
+symbols = sorted_df["Symbol"].values.tolist()
+no_data = ['BABA','HD','HDB','HSBC','PBR','RIO','SNY','UL','DXCM','LLY','KO','ORCL','BHP','T','UNP','DEO','BP','BTI','CM']
+flag = True
+prev_day_dict = {}
+for symbol in symbols[:101]:
+    print(symbol)
+    each_stock_df = pd.read_csv(os.path.join(stock_data_path, symbol+".csv"))
+    columns = each_stock_df.columns
+    columns = columns[:-1]
+    if flag:
+        prev_day_dict = previous_day_stock_details(symbol,each_stock_df,prev_day_dict)
+        flag = False
+    else:
+        prev_day_dict = previous_day_stock_details(symbol,each_stock_df,prev_day_dict)
+prev_df = pd.DataFrame.from_dict(prev_day_dict)
+if os.path.exists(os.path.join(path,"previous_day_stock_details.csv")):
+    os.remove(os.path.join(path,"previous_day_stock_details.csv"))
+prev_df.to_csv(os.path.join(path,"previous_day_stock_details.csv"))
